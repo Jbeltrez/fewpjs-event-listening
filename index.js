835240cc821c62e1703755cefe9b65a1be5512d5
Jbeltrez <beltrezjohn@gmail.com>
2021-06-14 19:42:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-function addingEventListener() {
-}
+
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+//     input.addEventListener('click', customAlert());  
+// }
+
+
+
+// addingEventListener(); 
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('DOM fully loaded and parsed');
+        
+    
+//         const input = document.getElementById('input');
+
+//         input.addEventListener('click', function() {
+//             alert('I was clicked!');
+  
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    let submit = document.getElementById('input')
+    submit.addEventListener('click', (e)=>{
+        //see line 64 for CreatQuote Function
+       alert('I was clicked!')
+    })
+})
